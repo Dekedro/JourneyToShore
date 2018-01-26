8e8e4c9a49e39c99109750f1e28e711029abee2d
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2018-01-27 00:14:14 +0200

    Add GUI Prefab

---
+++ Assets/Scripts/PauseButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour {
+	public GameObject menu;
+
+	bool game_paused = false;
+	public void Pause() {
+		if (game_paused) {
+			Time.timeScale = 1;
+			menu.SetActive (false);
+			game_paused = false;
+		} else {
+			Time.timeScale = 0;	
+			menu.SetActive (true);
+			game_paused = true;
+		}
+	}
+}
