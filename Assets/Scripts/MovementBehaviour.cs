8b0376af14e46810441df6037024ade9bac54f4c
Savas <tomas1savickas@protonmail.com>
2018-01-27 07:23:49 +0000

    Add ship movement

---
+++ Assets/Scripts/MovementBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour {
+	public float rotationSpeed;
+	public Quaternion targetRotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * rotationSpeed); 	
+	}
+}
