6efade4be59c65ce92508d38b88ab95f90b2ee2b
Savas <tomas1savickas@protonmail.com>
2018-01-27 12:03:15 +0000

    Add Camera following

---
+++ Assets/Scripts/CameraFollowingBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowingBehaviour : MonoBehaviour {
+	public GameObject target;
+
+	public float trackingSpeed = 1f;
+	public float damping = 1f;
+
+	private Vector3 _velocity = Vector3.zero;
+	private void Update () {
+		var destination = new Vector3 (target.transform.position.x, target.transform.position.y, transform.position.z); 
+		transform.position = Vector3.SmoothDamp(transform.position, destination, ref _velocity, trackingSpeed, damping); 
+	}
+}
