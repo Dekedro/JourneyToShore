ac68fcc12e9b2428dea2c23f249868590fddc8fc
Savas <tomas1savickas@gmail.com>
2018-01-27 20:23:22 +0000

    Increase depth instead of decreasing

--- Assets/Scripts/SubmarineBehaviour.cs
+++ Assets/Scripts/SubmarineBehaviour.cs
@@ -7,12 +7,13 @@ public class SubmarineBehaviour : MonoBehaviour {
 
 	private void Awake() {
 		Resources = new Dictionary<string, SubmarineResource> ();
-	}
-
-	private void Start () {
 		Resources.Add("Fuel", new SubmarineResource());
 		Resources.Add("Food", new SubmarineResource());
 		Resources.Add("Oxygen", new SubmarineResource());
-		Resources.Add("Depth", new SubmarineResource (0f, 1000f, 900f, 0.02f, true));
+		Resources.Add("Depth", new SubmarineResource (0f, 1000f, 100f, -0.02f, true));
+	}
+
+	private void Start () {
+		
 	}
 }
