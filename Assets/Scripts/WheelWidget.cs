3fc3475b790a8fd520168a0d971aca01c53eb308
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2018-01-27 15:43:28 +0200

    modify Smoth wheel (good joke)

--- Assets/Scripts/WheelWidget.cs
+++ Assets/Scripts/WheelWidget.cs
@@ -10,27 +10,47 @@ public class WheelWidget : Widget {
 
 	private void Start() {
 		_player = GameObject.FindGameObjectWithTag ("Player");
+		rotationSpeed = 0.05f;
 	}
 
 	private void OnMouseDown() {
 		StartCoroutine (FollowMouse());
 	}
 
+	//float rotationSpeed;
+
 	Vector3 lastMouseCoord;
 	private IEnumerator FollowMouse() {
+		Debug.Log ("nueina");
 		var diff = lastMouseCoord - Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		bool initialized = false;
+		float startAngle = 0;
+	
 		while(Input.GetMouseButton(0)) {
+			if (!initialized) {
+				initialized = true;
+				var point = Camera.main.ScreenToWorldPoint (Input.mousePosition);// + diff;
 
 
-			var point = Camera.main.ScreenToWorldPoint (Input.mousePosition);// + diff;
+				float AngleRad = Mathf.Atan2 (point.y - transform.position.y, point.x - transform.position.x);
+				float AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
+				startAngle = AngleDeg;
+				//Debug.Log (startAngle);
+			} else {
+				var point = Camera.main.ScreenToWorldPoint (Input.mousePosition);// + diff;
 
 
-			float AngleRad = Mathf.Atan2(point.y - transform.position.y, point.x - transform.position.x);
-			float AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
+				float AngleRad = Mathf.Atan2(point.y - transform.position.y, point.x - transform.position.x);
+				float AngleDeg = (180 / Mathf.PI) * AngleRad - 90;
+				//Debug.Log (AngleDeg);
+				Debug.Log (transform.rotation.z);
+				Debug.Log (AngleDeg - startAngle);
+				if (System.Math.Abs(AngleDeg - startAngle) > 0.1)
+					transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + (AngleDeg - startAngle)*rotationSpeed);
+				//transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (0, 0, AngleDeg), Time.deltaTime * rotationSpeed);
+				_player.GetComponent<MovementBehaviour> ().targetRotation = transform.rotation;
 
-			transform.rotation = Quaternion.Euler (0, 0, AngleDeg);
-			//transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (0, 0, AngleDeg), Time.deltaTime * rotationSpeed);
-			_player.GetComponent<MovementBehaviour> ().targetRotation = transform.rotation;
+			}
 			yield return null;
 		}
 		lastMouseCoord = Camera.main.ScreenToWorldPoint (Input.mousePosition);
