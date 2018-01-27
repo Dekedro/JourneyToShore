9c62400bcf4b010d15ff9c1e621fa1f67308718f
Savas <tomas1savickas@protonmail.com>
2018-01-27 08:02:57 +0000

    Add submarine's behaviour

---
+++ Assets/Scripts/ResourceConsumer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceConsumer : MonoBehaviour {
+
+	public bool consumeResources = true;
+
+	private IDictionary<string, SubmarineResource> _resources;
+
+	private void Start () {
+		_resources = GetComponent<SubmarineBehaviour>().Resources;		
+	}
+	
+	private void Update () {
+		foreach (var resource in _resources.Keys) {
+			_resources [resource].Consume ();
+		}
+	}
+}
