5ef7ecf026454caa7c1d9fca566a6a812ece37ef
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2018-01-27 10:43:42 +0200

    add Smooth Text prefab

---
+++ Assets/Scripts/SlidingBehaviour.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SlidingBehaviour : MonoBehaviour {
+	public Vector3 targetPosition;
+
+	public bool useLinearInterpolation;
+	public AnimationCurve movementCurve;
+
+	private Vector3 _startingPosition;
+	private bool _isAtOrigin = true;
+	private bool _movementPaused = false;
+	private Action<bool> _callback = null;
+
+	private void Awake() {
+		_startingPosition = transform.position;
+	}
+		
+	public void Update() {
+		Debug.Log (transform.position);
+	}
+
+	public void Toggle() {
+		if (_isAtOrigin) {
+			MoveToTarget ();
+		} else {
+			MoveToOrigin ();
+		}
+	}
+
+	public void TeleportToTarget() {
+		if (_isAtOrigin) {
+			StopAllCoroutines();
+			transform.position = targetPosition;
+			_isAtOrigin = false;
+		}
+	}
+
+	public void TeleportToOrigin() {
+		if (!_isAtOrigin) {
+			StopAllCoroutines();
+			transform.position = _startingPosition;
+			_isAtOrigin = true;
+		}
+	}
+
+	public void MoveToTarget() 
+	{
+		if (_isAtOrigin) {
+			StopAllCoroutines();
+			StartCoroutine(Move(transform.position, targetPosition, false));
+		}
+	}
+
+	public void MoveToOrigin() 
+	{
+		if (!_isAtOrigin) {
+			StopAllCoroutines();
+			StartCoroutine(Move(transform.position, _startingPosition, true));
+		}
+
+	}
+
+	private IEnumerator Move(Vector2 startPos, Vector2 targetPos, bool willBeAtOrigin) {
+		var startTime = Time.time;
+		while (((Vector2)transform.position - targetPos).magnitude > Time.deltaTime) {
+
+			var timePauseStarted = Time.time;
+			while (_movementPaused) {
+				yield return null;
+			}
+			startTime += Time.time - timePauseStarted;
+
+			if (useLinearInterpolation) {
+				transform.position = Vector3.Lerp(startPos, targetPos, Time.time - startTime);
+			} else {
+				var currentTime = Time.time - startTime;
+				var progress = Utils.Remap(
+					movementCurve.Evaluate(currentTime),
+					movementCurve.keys[0].value,
+					movementCurve.keys[movementCurve.length-1].value,
+					0,
+					1
+				);
+				transform.position = new Vector3(
+					Utils.Remap(progress, 0, 1, startPos.x, targetPos.x),
+					Utils.Remap(progress, 0, 1, startPos.y, targetPos.y));
+			}
+
+			transform.position = new Vector3(
+				transform.position.x,
+				transform.position.y,
+				_startingPosition.z);
+			yield return null;
+		}
+		_isAtOrigin = willBeAtOrigin;
+		if(_callback != null)
+			_callback(_isAtOrigin);
+	}
+
+	/// <summary>
+	/// Sets the callback.
+	/// </summary>
+	/// <param name="action">Action which takes a bool "isMovingToOrigin"</param>
+	public void SetCallback(Action<bool> action) {
+		_callback = action;
+	}
+
+	public void Pause() {
+		Debug.Log("pausing...");
+		_movementPaused = true;
+	}
+
+	public void Unpause() {
+		Debug.Log("unpausing...");
+		_movementPaused = false;
+	}
+}
---
+++ Assets/Scripts/TextSmooth.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSmooth : MonoBehaviour {
+	LinkedList<string> story;
+	// Use this for initialization
+	float scene_start;
+	//float last_text_printed;
+	bool stop = false;
+	string now_writing_text;
+	float writing_speed = 10;
+	void Start () {
+		scene_start = Time.time;
+		//last_text_printed = scene_start;
+		GetComponent<Text>().text = "";
+
+		story = new LinkedList<string>();
+		story.AddLast ("First text");
+		story.AddLast ("Second text");
+		story.AddLast ("Third text a lot a lot Third text a lot a lot Third text a lot a lot Third text a lot a lot Third text a lot a lot ");
+		now_writing_text = "FIRST TEXT";
+		StartCoroutine (Whatever ("FIRST TEXT"));//Whatever ();
+
+
+		/*
+		while (!(story.First == null)) {
+			string t = story.First.Value;
+			story.RemoveFirst ();
+			StartCoroutine (Whatever(t));
+		}
+		*/
+
+	}
+
+
+
+	private IEnumerator Whatever(string s) {
+		int n = 0;
+		while (n < s.Length && !stop && s==now_writing_text) {
+			//text += "a";
+			GetComponent<Text> ().text += s [n];
+			n++;
+			yield return new WaitForSeconds (Time.fixedDeltaTime * writing_speed);
+		}
+		stop = true;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		//GetComponent<Text> ().text;
+
+		if (stop && Input.GetKeyDown("space")) {
+			stop = false;
+			GetComponent<Text>().text = "";
+			if (story.First.Value != null) {
+				now_writing_text = story.First.Value;
+				StartCoroutine (Whatever (story.First.Value));
+				story.RemoveFirst ();
+			}
+
+		} 
+			
+
+
+	}
+
+
+	void FixedUpdate() {
+		//
+
+
+		if (Input.GetKeyDown ("space")) {
+			writing_speed = 0.8f;
+		}
+		if(Input.GetKeyUp("space")) {
+			writing_speed = 10f;
+		}//*/
+
+	}
+
+
+}
