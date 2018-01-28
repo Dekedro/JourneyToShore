4a6d3817857b16aa26d8cb4418a2eb1b827c7697
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2018-01-28 15:29:04 +0200

    add skip button story

---
+++ Assets/Scripts/ButtonBehaviours/SkipButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipButton : MonoBehaviour {
+
+	public GameObject v;
+	public void PressedDown() {
+		v.SetActive (false);
+		Time.timeScale = 1;
+	}
+}
